ff46cb31f6650b5e408de8fb6d76ad0643d9f5d1
Kacy Gilbert <gilbertkacy90@gmail.com>
2022-11-23 10:57:12 -0600

    equipped user.controller.js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -16,19 +16,51 @@ const UserController = {
     },
     //READ
     getAll: () => {
-        console.log("Get All")
+        console.log("Get All Users")
+        User
+            .find({})
+            .then((result) => {
+                res.json(result)
+            })
+            .catch((err) => {
+                res.status(404).json({ msg: "An error occurred attempting to retrieve all users", error: err })
+            })
     },
     getOne: () => {
-        console.log("Get All")
+        console.log("Get One User")
+        User
+            .findOne({ _id: req.params.id })
+            .then((result) => {
+                res.json(result)
+            })
+            .catch((err) => {
+                res.status(404).json({ msg: "An error occurred attempting to retrieve User.", error: err })
+            })
     },
     //UPDATE
     update: () => {
-        console.log("Get All")
+        console.log("Update User")
+        User
+            .findOneAndUpdate({ _id: req.params.id }, req.body, { new: true, runValidators: true })
+            .then((result) => {
+                res.json(result)
+            })
+            .catch((err) => {
+                res.status(404).json({ msg: "An Error occurred attempting to update User", error: err })
+            })
     },
 
     //DELETE
     delete: () => {
-        console.log("Get All")
+        console.log("Delete User")
+        User
+            .findOneAndDelete({ _id: req.params.id })
+            .then((result) => {
+                res.json(result)
+            })
+            .catch((err) => {
+                res.status(404).json({ msg: "An error occurred when trying to delete user:", error: err })
+            })
     }
 }
 
