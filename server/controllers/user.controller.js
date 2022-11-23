1bb8d3ec77cd1ef0715532b6b43235b2aa233fa6
Kacy Gilbert <gilbertkacy90@gmail.com>
2022-11-23 10:22:56 -0600

    setting up

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,25 @@
+
+
+const UserController = {
+
+    //CREATE
+    create: () => {
+        console.log("Create")
+    },
+    //READ
+    getAll: () => {
+        console.log("Get All")
+    },
+    getOne: () => {
+        console.log("Get All")
+    },
+    //UPDATE
+    update: () => {
+        console.log("Get All")
+    },
+    
+    //DELETE
+    delete: () => {
+        console.log("Get All")
+    }
+}
